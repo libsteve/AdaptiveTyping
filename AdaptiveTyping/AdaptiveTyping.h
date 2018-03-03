058635afe59c265293f0f9ff83ec51f8a7c585c0
Steven Brunwasser <steve@brun.ws>
2018-03-02 17:09:32 -0800

    Initial Commit

---
+++ AdaptiveTyping/AdaptiveTyping.h
@@ -0,0 +1,19 @@
+//
+//  AdaptiveTyping.h
+//  AdaptiveTyping
+//
+//  Created by Steve Brunwasser on 3/2/18.
+//  Copyright © 2018 Steve Brunwasser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdaptiveTyping.
+FOUNDATION_EXPORT double AdaptiveTypingVersionNumber;
+
+//! Project version string for AdaptiveTyping.
+FOUNDATION_EXPORT const unsigned char AdaptiveTypingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdaptiveTyping/PublicHeader.h>
+
+
